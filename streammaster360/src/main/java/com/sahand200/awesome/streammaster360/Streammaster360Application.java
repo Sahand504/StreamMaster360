39cec92b2cd0821ca3f5561c1a2ccf88953f5836
Sahand Asri <sahandasri@gmail.com>
2024-05-28 23:06:44 -0400

    Initial Commit - Hooray :)

---
+++ streammaster360/src/main/java/com/sahand200/awesome/streammaster360/Streammaster360Application.java
@@ -0,0 +1,13 @@
+package com.sahand200.awesome.streammaster360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Streammaster360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Streammaster360Application.class, args);
+	}
+
+}
